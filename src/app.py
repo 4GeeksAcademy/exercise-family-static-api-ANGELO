38c85a655a31cd5d2004754d1ed4820c8b2f8690
zebhastyanangelo-svg <zebhastyanangelo@gmail.com>
2026-06-13 20:25:20 +0000

    feat: Implement get_member and delete_member methods in Family class

--- src/app.py
+++ src/app.py
@@ -41,20 +41,25 @@ def new_member():
     body = request.get_json()
     if not body or 'first_name' not in body or 'age' not in body or 'lucky_numbers' not in body:
         return jsonify({'error': 'Missing required fields: first_name, age, lucky_numbers'}), 400
-    
-    new_id = jackson_family._generate_id()
-    new_member = {
-        'id': new_id,
-        'first_name': body['first_name'],
-        'last_name': jackson_family.last_name,
-        'age': body['age'],
-        'lucky_numbers': body['lucky_numbers']
-    }
-    
-    jackson_family._members.append(new_member)
-    
-    return jsonify(new_member), 201   
-            
+    new_member = jackson_family.add_member(body)
+
+    return jsonify(new_member), 200
+
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    if member is None:
+        return jsonify({'error': 'Member not found'}), 404
+    else:
+        return jsonify(member), 200
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    deleted = jackson_family.delete_member(id)
+    if deleted is None:
+        return jsonify({'error': 'Member not found'}), 404
+    return jsonify({'done': True}), 200
+
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/datastructures.py
+++ src/datastructures.py
@@ -5,19 +5,35 @@ Update this file to implement the following already declared methods:
 - get_member: Should return a member from the self._members list
 """
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
         self._next_id = 1
         self._members = [
             {
-                "id": self._generate_id(),
-                "first_name": "John",
-                "last_name": last_name,
-                "age": 33,
-                "lucky_numbers": [7, 13, 22]
+                'id': self._generate_id(),
+                'first_name': 'John',
+                'last_name': last_name,
+                'age': 33,
+                'lucky_numbers': [7, 13, 22]
+            },  
+            {
+                'id': self._generate_id(),
+                'first_name': 'Jane',
+                'last_name': last_name,
+                'age': 35,
+                'lucky_numbers': [10, 14, 13]
+            },  
+            {
+                'id': self._generate_id(),
+                'first_name': 'Jimy', 
+                'last_name': last_name,
+                'age': 5,  
+                'lucky_numbers': [1]  
             }
         ]
+        
 
     # This method generates a unique incremental ID
     def _generate_id(self):
@@ -26,19 +42,32 @@ class FamilyStructure:
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+        if 'id' not in member or member['id'] is None:
+            member['id'] = self._generate_id()
+
+        if 'last_name' not in member:
+            member['last_name'] = self.last_name
+
+        self._members.append(member)
+        return member
+    
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        # You have to implement this method
+        # Loop the list and delete the member with the given id
+        for i, member in enumerate(self._members):
+            if member.get('id') == id:
+                del self._members[i]
+                return {'done': True}
+        return {'done': False}
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        # You have to implement this method
+        # Loop all the members and return the one with the given id
+        for member in self._members:
+            if member.get('id') == id:
+                return member
+        return None
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
